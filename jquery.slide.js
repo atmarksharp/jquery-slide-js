b856fa30786213e8341bfb12f87a4070242c806d
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2014-12-23 20:17:22 +0900

    v0.02

--- jquery.slide.js
+++ jquery.slide.js
@@ -2,25 +2,32 @@ jQuery.fn.extend({
   slide: function(params){
     var $ = jQuery;
     var param = params || {};
-    var $parent = $(this);
-    var $images = $parent.find('img');
-    var w = param['width'] || $($images[0]).width() + 'px';
-    var h = param['height'] || $($images[0]).height() + 'px';
-    var background = param['background'] || '#ffffff';
-    $parent.css({'width':w,'height':h,'background':background,'overflow':'hidden','position':'relative'});
-    $images.removeAttr('hidden').fadeOut(0);
-    $($images[$images.length-1]).fadeIn(0);
+    var $slide = $(this);
+    var images = [];
+    $slide.find('li').each(function(){
+      images.push($(this).attr('data-src'));
+    });
+    $slide.html('');
 
-    var fadeTime = param['fadeIn'] || 1000;
-    var delayTime = param['delay'] || 1000;
+    var __back = $('<div id="sl-back"></div>');
+    var __front = $('<img src="" id="sl-front"/>');
+    __back.append(__front);
+    $slide.append(__back);
+
+    var getBack = function(){ return $($slide.children()[0]) };
+    var getFront = function(){ return $(getBack().children()[0]) };
+
+    var fadeTime = +(param['fadeIn']) || 1000;
+    var delayTime = +(param['delay']) || 1000;
     var waitTime = fadeTime + delayTime;
 
-    var index = 0
-    var z = 0
+    var i = 0
     setInterval(function(){
-      $($images[index]).css({'position':'absolute','z-index':''+z}).fadeIn(fadeTime).delay(waitTime).fadeOut(0);
-      ++z;
-      if(++index == $images.length){ index = 0 }
-    }, waitTime);
+      n1 = i%images.length;
+      n2 = (i+1)%images.length;
+      getBack().css('background-image','url('+images[n1]+')');
+      getFront().attr('src',images[n2]).hide().fadeIn(fadeTime);
+      i++;
+    }, waitTime)
   }
 });
